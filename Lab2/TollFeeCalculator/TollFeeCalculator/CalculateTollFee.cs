2c55a5d484eee27032cdc2f43b605a4ae13fa87c
AndersOrlinder <anders_orlinder@hotmail.com>
2020-12-02 14:25:34 +0100

    Update refactor TollFeePass method

--- Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TollFeeCalculator
 {
@@ -9,20 +10,20 @@ namespace TollFeeCalculator
             run(Environment.CurrentDirectory + "../../../../testData.txt");
         }
 
-        public static void run(String inputFile) {
-            string indata = System.IO.File.ReadAllText(inputFile);
-            String[] dateStrings = indata.Split(", ");
-            DateTime[] dates = new DateTime[dateStrings.Length-1];
+        public static void run(String passingDatesFile) {
+            string passingDatesString = System.IO.File.ReadAllText(passingDatesFile);
+            String[] passingDatesStringArray = passingDatesString.Split(", ");
+            DateTime[] dates = new DateTime[passingDatesStringArray.Length-1];
             for(int i = 0; i < dates.Length; i++) {
-                dates[i] = DateTime.Parse(dateStrings[i]);
+                dates[i] = DateTime.Parse(passingDatesStringArray[i]);
             }
             Console.Write("The total fee for the inputfile is " + TotalFeeCost(dates));
         }
 
-        public static int TotalFeeCost(DateTime[] d) {
+        public static int TotalFeeCost(DateTime[] dateTimes) {
             int fee = 0;
-            DateTime si = d[0]; //Starting interval
-            foreach (var d2 in d)
+            DateTime si = dateTimes[0]; //Starting interval
+            foreach (var d2 in dateTimes)
             {
                 long diffInMinutes = (d2 - si).Minutes;
                 if(diffInMinutes > 60) {
@@ -35,26 +36,104 @@ namespace TollFeeCalculator
             return Math.Max(fee, 60);
         }
 
-        public static int TollFeePass(DateTime d)
+        public static int TollFeePass(DateTime dateTime)
         {
-            if (free(d)) return 0;
-            int hour = d.Hour;
-            int minute = d.Minute;
-            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-            else return 0;
+            if (free(dateTime)) return 0;
+            int timeAsDecimal = dateTime.Hour + dateTime.Millisecond / 60;
+            int hour = dateTime.Hour;
+            int minute = dateTime.Minute;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0000_TO_0600]) return 0;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0600_TO_0630]) return 8;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0630_TO_0700]) return 13;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0700_TO_0800]) return 18;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0800_TO_0830]) return 13;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_0830_TO_1500]) return 8;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_1500_TO_1530]) return 13;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_1530_TO_1700]) return 18;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_1700_TO_1800]) return 13;
+            if (dateTime < passingSchedule[FeeTimePeriod.FR_1800_TO_1830]) return 8;
+            return 0;
+        }
+
+
+        private static readonly Dictionary<FeeTimePeriod, DateTime> passingSchedule = new Dictionary<FeeTimePeriod, DateTime>()
+        {
+            {FeeTimePeriod.FR_0000_TO_0600, new DateTime(2020,01,01,06,00,00)},//0
+            {FeeTimePeriod.FR_0600_TO_0630, new DateTime(2020,01,01,06,30,00)},//8
+            {FeeTimePeriod.FR_0630_TO_0700, new DateTime(2020,01,01,07,00,00)},//13
+            {FeeTimePeriod.FR_0700_TO_0800, new DateTime(2020,01,01,08,00,00)},//18
+            {FeeTimePeriod.FR_0800_TO_0830, new DateTime(2020,01,01,08,30,00)},//13
+            {FeeTimePeriod.FR_0830_TO_1500, new DateTime(2020,01,01,15,00,00)},//8
+            {FeeTimePeriod.FR_1500_TO_1530, new DateTime(2020,01,01,15,30,00)},//13
+            {FeeTimePeriod.FR_1530_TO_1700, new DateTime(2020,01,01,17,00,00)},//18
+            {FeeTimePeriod.FR_1700_TO_1800, new DateTime(2020,01,01,18,00,00)},//13
+            {FeeTimePeriod.FR_1800_TO_1830, new DateTime(2020,01,01,18,30,00)},//8
+
+        };
+
+        enum FeeTimePeriod
+        {
+            FR_0000_TO_0600,
+            FR_0600_TO_0630,
+            FR_0630_TO_0700,
+            FR_0700_TO_0800,
+            FR_0800_TO_0830,
+            FR_0830_TO_1500,
+            FR_1500_TO_1530,
+            FR_1530_TO_1700,
+            FR_1700_TO_1800,
+            FR_1800_TO_1830,
         }
 
         //Gets free dates
-        public static bool free(DateTime day) {
-        return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7;
+        public static bool free(DateTime dateTime) {
+            return (int)dateTime.DayOfWeek == 5 || (int)dateTime.DayOfWeek == 6 || dateTime.Month == 7;
+        }
+
+        enum FeeTimePeriod123
+        {
+            Fr_0600_To_0629=0,
+            Fr_0630_To_0659=1,
+            Fr_0700_To_0759=2,
+        }
+
+        private static readonly Dictionary<FeeTimePeriod123, Tuple<DateTime,DateTime,int>>
+            passingSchedules123 = new Dictionary<FeeTimePeriod123, Tuple<DateTime, DateTime,int>>()
+        {
+            {
+                FeeTimePeriod123.Fr_0600_To_0629, 
+                new Tuple<DateTime,DateTime,int>(
+                    new DateTime(2020,01,01,06,00,00),
+                    new DateTime(2020,01,01,06,29,59),
+                    8)
+            },
+            {
+                FeeTimePeriod123.Fr_0630_To_0659,
+                new Tuple<DateTime,DateTime,int>(
+                    new DateTime(2020,01,01,06,30,00),
+                    new DateTime(2020,01,01,06,59,00),
+                    13)
+            },
+            {
+                FeeTimePeriod123.Fr_0700_To_0759,
+                new Tuple<DateTime,DateTime,int>(
+                    new DateTime(2020,01,01,07,00,00),
+                    new DateTime(2020,01,01,07,59,00),
+                    18)
+            }
+        };
+
+        public static int TollFeePass123(DateTime dateTime)
+        {
+            int fee = 0;
+            for (int i = 0; i < passingSchedules123.Count; i++)
+            {
+                if (dateTime.IsWithinTimeInterval(passingSchedules123[FeeTimePeriod123[i]]))
+                {
+                    
+                }
+            }
+            return fee;
         }
     }
 }
--- Lab2/TollFeeCalculator/TollFeeCalculatorTests/CalculateTollFeeTests.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTests/CalculateTollFeeTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using TollFeeCalculator;
+using System.Diagnostics;
 
 namespace TollFeeCalculatorTests
 {
@@ -20,11 +21,11 @@ namespace TollFeeCalculatorTests
                     Environment.CurrentDirectory +
                     "../../../../../TollFeeCalculator/testData.txt"
                 );
-                var expected  = "The total fee for the inputfile is 55";
+                var expected  = "The total fee for the input file is 55";
                 Assert.AreEqual(expected, stringWriter.ToString());
             }
         }
-
+        
         [TestMethod]
         public void TotalFeeCostTest()
         {
@@ -52,7 +53,7 @@ namespace TollFeeCalculatorTests
         [DynamicData(nameof(GetDatesAndPrices), DynamicDataSourceType.Method)]
         public void TullFeePassTest(string dateString, int fee)
         {
-            var date = DateTime.ParseExact(dateString, "yyyy,MM,dd,HH,mm", CultureInfo.InvariantCulture);
+            var date = DateTime.ParseExact(dateString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
             var expected = fee;
             var actual = CalculateTollFee.TollFeePass(date);
 
@@ -61,36 +62,35 @@ namespace TollFeeCalculatorTests
 
         internal static IEnumerable<object[]> GetDatesAndPrices()
         {
-            yield return new object[] { "2020,06,30,00,50", 0 };
-            yield return new object[] { "2020,06,30,06,34", 13 };
-            yield return new object[] { "2020,06,30,08,52", 8 };
-            yield return new object[] { "2020,06,30,10,13", 8 };
-            yield return new object[] { "2020,06,30,10,25", 8 };
-            yield return new object[] { "2020,06,30,11,04", 8 };
-            yield return new object[] { "2020,06,30,16,50", 18 };
-            yield return new object[] { "2020,06,30,18,00", 8 };
-            yield return new object[] { "2020,06,30,21,30", 0 };
-            yield return new object[] { "2020,07,01,00,00", 0 };
+            yield return new object[] { "2020-06-30 00:05", 0 };
+            yield return new object[] { "2020-06-30 06:34", 13 };
+            yield return new object[] { "2020-06-30 08:52", 8 };
+            yield return new object[] { "2020-06-30 10:13", 8 };
+            yield return new object[] { "2020-06-30 10:25", 8 };
+            yield return new object[] { "2020-06-30 11:04", 8 };
+            yield return new object[] { "2020-06-30 16:50", 18 };
+            yield return new object[] { "2020-06-30 18:00", 8 };
+            yield return new object[] { "2020-06-30 21:30", 0 };
+            yield return new object[] { "2020-07-01 00:00", 0 };
         }
-
+       
         [TestMethod]
         [DynamicData(nameof(GetFreePassDates), DynamicDataSourceType.Method)]
         public void FreeTest(string dateString, bool isFreeDate)
         {
-            var date = DateTime.ParseExact(dateString, "yyyy,MM,dd,HH,mm", CultureInfo.InvariantCulture);
+            var date = DateTime.ParseExact(dateString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
             var expected = isFreeDate;
             var actual = CalculateTollFee.free(date);
-
+            Trace.WriteLine("Error from input: " + dateString);
             Assert.AreEqual(expected, actual);
         }
 
         internal static IEnumerable<object[]> GetFreePassDates()
         {
-            yield return new object[] { "2020,07,06,16,10", true };
-            yield return new object[] { "2020,12,02,10,13", false }; 
-            yield return new object[] { "2020,12,18,08,34", false }; // Friday is fee weekday
-            yield return new object[] { "2020,12,20,08,34", true }; // Sunday is fee free weekday
-            yield return new object[] { "2020,12,25,13,52", true }; // Christmas day is always a Sunday
+            yield return new object[] { "2020-07-06 16:10", true };
+            yield return new object[] { "2020-12-02 10:13", false };
+            yield return new object[] { "2020-12-18 08:34", false };
+            yield return new object[] { "2020-12-20 08:34", true };
         }
     }
 }
